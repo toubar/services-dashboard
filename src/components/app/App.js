9d4d16b98b82fe5f08ec1136be76333aadb98e19
Toubar <aatoubar@gmail.com>
2020-01-29 16:08:37 +0100

    - Fetch data from the Flask endpoint instead of Mock Server - Styling

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -6,30 +6,36 @@ const App = () => {
 
     // useState is a Hook that lets you add state to function components.
     const [services, setServices] = useState([]);
-
-    // TODO - call the Flask endpoint when it's done
-    const fetchData = async () => {
-        const res = await fetch('http://localhost:3003/services');
-        res
-            .json()
-            .then(response => setServices(response))
-    };
+    const [displayData, setDisplayData] = useState(false);
 
     // useEffect Hook - analogous to Life Cycle Methods for class based components i.e. componentWillMount()
     useEffect(() => {
+        const fetchData = async () => {
+            const res = await fetch('http://127.0.0.1:5000/services/');
+            if (res.status === 200) {
+                res.json().then((res) => {
+                    setServices(res['services']);
+                    setDisplayData(!displayData);
+                })
+            }
+        };
         fetchData();
     }, []);
 
     return (
-        <div className="Services-container">
-            {services.map((item, i) => {
-                return (
-                    <div className="Service" key={i}>
-                        <Service service={item}/>
-                    </div>
-                )
-            })}
-        </div>
+        displayData ?
+            <div className="Services-container">
+                {services.map((item, i) => {
+                    return (
+                        <div className="Service" key={i}>
+                            <Service service={item}/>
+                        </div>
+                    )
+                })
+                }
+            </div>
+            :
+            <h1>Internal server error</h1>
     );
 };
 
